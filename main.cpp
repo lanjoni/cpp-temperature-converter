9da432883f1f45a0b78068424251e3e021226b1d
guto <91899995+gutoso@users.noreply.github.com>
2022-04-05 18:08:20 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    float f, c;
+
+    cout << "\n Informe a temperatura em graus Fahrenheit: ";
+    cin >> f;
+
+    c = (f - 32) * (5.0 / 9.0);
+
+    cout << "\n A temperatura informada em graus Celsius é: " << c << endl;
+}
